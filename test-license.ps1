347f31ae188a697fccabdbcc4458b9200dc6ccd6
Adam Patridge <patridge@gmail.com>
2018-06-10 22:24:23 -0600

    Rename all to 'add-*'.

--- test-license.ps1
+++ test-license.ps1
@@ -6,10 +6,10 @@ nuget pack ./dotnet-new-template-creator.nuspec
 # Install the new package
 dotnet new --install ./dotnet-new-template-creator.0.0.1.nupkg
 # Clear out our test output location
-Remove-Item -Path test -Force -Recurse
+Get-ChildItem test -Recurse | Remove-Item -Force
 
 # Spin up a new license: MIT
-dotnet new repo-license --name test --license MIT --fullName "Someone SuperAwesome" --copyrightYear 2020
+dotnet new add-license --name test --license MIT --fullName "Someone SuperAwesome" --copyrightYear 2020
 # Dump first three lines of resulting LICENSE file
 Get-Content -TotalCount 3 test/LICENSE
 # List contents of test output location (make sure nothing more than LICENSE)
@@ -18,13 +18,13 @@ Get-ChildItem -Path test
 Get-ChildItem -Path test -Include * -File -Recurse | foreach { $_.Delete() }
 
 # Repeat for Apache and GPL
-dotnet new repo-license --name test --license Apache --fullName "Someone SuperAwesome" --copyrightYear 2020
+dotnet new add-license --name test --license Apache --fullName "Someone SuperAwesome" --copyrightYear 2020
 Get-Content -TotalCount 3 test/LICENSE
 Get-ChildItem -Path test
 Get-ChildItem -Path test -Include * -File -Recurse | foreach { $_.Delete() }
-dotnet new repo-license --name test --license GPL --fullName "Someone SuperAwesome" --copyrightYear 2020
+dotnet new add-license --name test --license GPL --fullName "Someone SuperAwesome" --copyrightYear 2020
 Get-Content -TotalCount 3 test/LICENSE
 Get-ChildItem -Path test
 
 # Clean up after ourselves
-Remove-Item -Path test -Force -Recurse
+Get-ChildItem test -Recurse | Remove-Item -Force
