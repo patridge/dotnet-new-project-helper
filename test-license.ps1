1a0c0a5b33af01e465c1b2c888cb512d72b68ec8
Adam Patridge <patridge@gmail.com>
2018-06-10 22:02:48 -0600

    License: switch test script to raw PowerShell to keep macOS happy.

--- test-license.ps1
+++ test-license.ps1
@@ -1,19 +1,17 @@
 Set-PSDebug -Trace 1
 dotnet new --uninstall dotnet-new-template-creator
-nuget pack .\dotnet-new-template-creator.nuspec
-dotnet new --install .\dotnet-new-template-creator.0.0.1.nupkg
-cd test
-rm -r *
-dotnet new repo-license --license MIT
-cat -TotalCount 3 LICENSE
-ls
-rm -r *
-dotnet new repo-license --license Apache
-cat -TotalCount 3 LICENSE
-ls
-rm -r *
-dotnet new repo-license --license GPL
-cat -TotalCount 3 LICENSE
-ls
-rm -r *
-cd ..
+nuget pack ./dotnet-new-template-creator.nuspec
+dotnet new --install ./dotnet-new-template-creator.0.0.1.nupkg
+Get-ChildItem -Path test -Include * -File -Recurse | foreach { $_.Delete() }
+dotnet new repo-license --name test --license MIT --fullName "Someone SuperAwesome" --copyrightYear 2020
+Get-Content -TotalCount 3 test/LICENSE
+Get-ChildItem -Path test
+Get-ChildItem -Path test -Include * -File -Recurse | foreach { $_.Delete() }
+dotnet new repo-license --name test --license Apache --fullName "Someone SuperAwesome" --copyrightYear 2020
+Get-Content -TotalCount 3 test/LICENSE
+Get-ChildItem -Path test
+Get-ChildItem -Path test -Include * -File -Recurse | foreach { $_.Delete() }
+dotnet new repo-license --name test --license GPL --fullName "Someone SuperAwesome" --copyrightYear 2020
+Get-Content -TotalCount 3 test/LICENSE
+Get-ChildItem -Path test
+Get-ChildItem -Path test -Include * -File -Recurse | foreach { $_.Delete() }
